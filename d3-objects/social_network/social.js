9415ad18c82298d00b5fb1f861ffd868eb3f0eec
ahrke <david.duy.minh@gmail.com>
2019-07-24 17:58:24 +0000

    completed unrequiretedFollowers()

--- d3-objects/social_network/social.js
+++ d3-objects/social_network/social.js
@@ -35,9 +35,12 @@ const twits = {
       follows: ["f05"]
     }
   },
+  dataAsArray: function() {
+    return Object.keys(this.data);
+  },
   getUserByName: function(name) {
     let user = {};
-    for (let i of Object.keys(this.data)) {
+    for (let i of this.dataAsArray()) {
       if (this.data[i].name === name) user = this.data[i];
     }
 
@@ -45,25 +48,22 @@ const twits = {
   },
   getUserByAccount: function(account) {
     let acc = {};
-    for (let i of Object.keys(this.data)) {
+    for (let i of this.dataAsArray()) {
       if (i === account) acc = this.data[i]
     }
 
     return acc;
   },
   biggestFollower: function() {
-    return Object.keys(this.data)
+    return this.dataAsArray()
       .map(user => [this.data[user].follows.length,this.data[user].name])
       .sort(sorter())
       .reverse()[0][1];
-  }, 
-  selectAccountData: function(account, selector) {
-    return this.getUserByAccount(account)[selector]
   },
   getFollowers: function(account) {
     let followers = [];
 
-    for (let i of Object.keys(this.data)) {
+    for (let i of this.dataAsArray()) {
       if (
         this.getUserByAccount(i).follows.includes(account) 
         && !followers.includes(this.getUserByAccount(i))
@@ -77,7 +77,7 @@ const twits = {
   mostPopular: function() {
     let numberOfFollowersList = [];
 
-    for (let user of Object.keys(this.data)) {
+    for (let user of this.dataAsArray()) {
       this.data[user].follows.forEach(account => {
         numberOfFollowersList[account]
           ? numberOfFollowersList[account]++
@@ -90,27 +90,40 @@ const twits = {
   },
   printAll: function() {
     // outputs a list of everyone and for each of them, the names of who they follow and who follows them
-    Object.keys(this.data).forEach(acc => {
+    this.dataAsArray().forEach(acc => {
       let user = this.data[acc]
       console.log(`${user.name}`)
       console.log(` Follows: ${user.follows}`)
-      console.log(` Followed By: `)
+      console.log(` Followed By: ${this.getFollowers(acc).join(', ')}`)
     })
+  },
+  unrequitedFollowers: function() {
+    // returns a list of names for those who follow someone that doesn't follow them back
+    let unrequited = [];
+
+    this.dataAsArray().forEach(acc => {
+      this.dataAsArray().forEach(innerAcc => {
+        if(this.getUserByAccount(acc).follows.includes(innerAcc) 
+          && !this.getUserByAccount(innerAcc).follows.includes(acc)
+          && !unrequited.includes(this.getUserByAccount(acc).name)) {
+            unrequited.push(this.getUserByAccount(acc).name)
+          }
+      })
+    })
+
+    return unrequited;
   }
 }
 
 
 
-const unrequitedFollowers = function() {
-
-}
 
 console.log(twits.biggestFollower());
 console.log(twits.getUserByName('Debbie').follows);
 console.log(twits.mostPopular())
 twits.printAll()
-console.log(twits.selectAccountData('f02','follows'))
 console.log(twits.getFollowers('f06'))
+console.log(twits.unrequitedFollowers())
 // Identify who has the most followers over 30
 // Identify who follows the most people over 30
 // List everyone and their reach (sum of # of followers and # of followers of followers)
