a5a24a08da9fba78a21fb147165ee795737a9bb9
ahrke <david.duy.minh@gmail.com>
2019-07-24 20:32:24 +0000

    added findKey.js

---
+++ lotide/findKey.js
@@ -0,0 +1,34 @@
+const assertEqual = require('./assertEqual').assertEqual;
+
+const findKey = function(obj, callback) {
+  for (let i in obj) {
+    if(callback(obj[i])) return i;
+  }
+};
+
+// assertEqual(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 3), 'Akaleri')
+
+// assertEqual(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2), 'noma')
+
+// assertEqual(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 1), 'Blue Hill')
