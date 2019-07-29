f8213f15ca74f187a72e8d06fd7a1afee353b7eb
ahrke <david.duy.minh@gmail.com>
2019-07-29 19:38:10 +0000

    added index.js

--- lotide/tail.js
+++ lotide/tail.js
@@ -1,14 +1,6 @@
-const assertEqual = require('./assertEqual').assertEqual;
-const assertArraysEqual = require('./assertArraysEqual').assertArraysEqual;
-
 let tail = function(arr) {
   return arr.slice(1);
 };
-
-assertArraysEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
-assertArraysEqual(tail([1]), []);
-assertArraysEqual(tail(['apples', 'oranges', 'berries', 'hoomans']), ['hoomans']);
-assertArraysEqual(tail([]), []);
 
 module.exports = {
   tail
--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,10 +1,22 @@
 const tail = require('../tail').tail;
-const assertArraysEqual = require('../assertArraysEqual').assertArraysEqual;
+const expect = require('chai').expect;
 
 describe("tail", () => {
   it("should return true if output is equal to array elements following first element", () => {
     let input = [1,2,3,4,5];
     let output = [2,3,4,5];
-    assertArraysEqual(tail(input),output)
+    expect(tail(input)).to.eql(output);
+  })
+
+  it("should return false when output is the head of the array", () => {
+    let input = [1,2,3,4,5];
+    let output = [1];
+    expect(tail(input)).to.not.eql(output);
   })
 })
+
+
+// assertArraysEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+// assertArraysEqual(tail([1]), []);
+// assertArraysEqual(tail(['apples', 'oranges', 'berries', 'hoomans']), ['hoomans']);
+// assertArraysEqual(tail([]), []);
